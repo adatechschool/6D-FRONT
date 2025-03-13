5ad2d254f1c7c2484c4903f0a94e8030f0f6765f
flo <boireaufloriane@hotmail.fr>
2025-03-13 10:24:26 +0100

    header quasi pret avce tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import React from 'react';
+// import du composant header dans la l'app.jsx qui est le point de départ de l'app
 import Header from './Header';
 
 function App() {
 
   return (
+    // <> div root, syntaxe jsx de react
     <>
+    {/* div principal de l'appli */}
     <div className='w-full h-full absolute bg-orange-50'>
+    {/* import du composant header */}
       <Header/>
     </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+// ce code configure et rend une application React en utilisant
+// le composant App comme point d'entrée, tout en activant le StrictMode pour aider au développement.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
