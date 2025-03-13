fa7e8a941a60936e1b4464461d13614b249b9a90
flo <boireaufloriane@hotmail.fr>
2025-03-13 14:52:15 +0100

    ajout de la carte main et de la main sur les boutons

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,23 +2,23 @@ function NavBar(){
     return (
         <>
         <nav className="text-orange-50 w-full h-15 bg-emerald-700">
-           <ul className="flex gap-5 justify-center py-3 px-2 md:px-32">
-           <li>
-            Tous nos meubles
-            </li> 
-            <p>|</p>
-            <li>
-            Nouveautés
-            </li>
-            <p>|</p>
-            <li>
-            Tables
-            </li> 
-            <p>|</p> 
-            <li>
-            Chaises
-            </li>
-           </ul>
+            <ul className="flex gap-5 justify-center py-3 px-2 md:px-32">
+                <button className="bg-transparent border-none hover:scale-90 cursor-grab text-orange-50 focus:outline-none">
+                    Tous nos meubles
+                </button>
+                <span className="text-orange-50">|</span>
+                <button className="bg-transparent border-none hover:scale-90 cursor-grab text-orange-50 focus:outline-none">
+                    Nouveautés
+                </button>
+                <span className="text-orange-50">|</span>
+                <button className="bg-transparent border-none hover:scale-90 cursor-grab text-orange-50 focus:outline-none">
+                    Tables
+                </button>
+                <span className="text-orange-50">|</span>
+                <button className="bg-transparent border-none hover:scale-90 cursor-grab text-orange-50 focus:outline-none">
+                    Chaises
+                </button>
+            </ul>
         </nav>
         </>
 
