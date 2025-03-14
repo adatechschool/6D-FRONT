6cddd3d31ad2acae8fc6e707003d3ec7d43e914c
Dorothee <braud.dorothee@gmail.com>
2025-03-14 15:18:46 +0100

    ajout searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-
-import React from 'react';
-// import du composant header dans la l'app.jsx qui est le point de départ de l'app
+import React, { useState } from 'react';
 import Header from './Header';
-import NavBar from './NavBar';
 import MainHomePage from './MainHomePage';
+
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
 
   return (
-    // <> div root, syntaxe jsx de react
-    <>
-    {/* div principal de l'appli */}
     <div className='w-full h-full absolute bg-orange-50'>
-    {/* import du composant header */}
-      <Header/>
-      <NavBar/>
-      <MainHomePage/>
+      <Header setSearchResults={setSearchResults} />
+      <MainHomePage searchResults={searchResults} />
     </div>
-    </>
-  )
+  );
 }
-export default App
+
+export default App;
