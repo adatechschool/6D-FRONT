c6abed0c18f3c975d0e7f8e5228e7c80bfed9b3a
Eloi RM <eloimailmaynad@gmail.com>
2025-03-19 12:21:47 +0100

    fixed picture url

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import like from './assets/like_2794617.svg';
-import fauteuil from './assets/fauteuil.webp';
 import { useParams } from 'react-router-dom';
 
 
@@ -19,16 +18,28 @@ function DetailPage() {
         .catch(error => console.error("C'est nous, Erreur lors du chargement du JSON:", error));
     }, []);
 
-
+    const [picture, setPicture] = useState({});
+    
+    useEffect(() => {
+        fetch(`http://localhost:8000/pictures/first/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            setPicture(data);
+            console.log('Picture Données chargées:', data);
+        })
+        .catch(error => console.error("C'est nous, Erreur lors du chargement du JSON:", error));
+    }, []);
+    console.log(picture.url);
+    
     return (
         <div className="flex flex-col md:flex-row items-start md:items-center md:space-x-6 space-y-6 md:space-y-0 mb-8 px-4 md:px-6">
             {/* Photo Card */}
             <div className="bg-white rounded-2xl shadow-md w-full md:w-1/2 p-4">
                 <h3 className="text-xl font-bold text-emerald-700 mb-4 font-primary">Photo</h3>
-                <img src={fauteuil} alt="fauteuil" className="w-full h-64 object-cover rounded-lg mb-6" />
+                <img src={picture.url} alt="photo meuble" className="w-full h-64 object-contain rounded-lg mb-6" />
                 <div className="flex justify-between">
                     {/* Navigation arrows */}
-                    <button className="bg-gray-200 hover:bg-gray-300 text-gray-500 hover:text-gray-700 rounded-full w-10 h-10 flex items-center justify-center transition-all">
+                     <button className="bg-gray-200 hover:bg-gray-300 text-gray-500 hover:text-gray-700 rounded-full w-10 h-10 flex items-center justify-center transition-all">
                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
                     </button>
                     <button className="bg-gray-200 hover:bg-gray-300 text-gray-500 hover:text-gray-700 rounded-full w-10 h-10 flex items-center justify-center transition-all">
